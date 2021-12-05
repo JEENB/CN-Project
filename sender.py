954c15c8ba85d07b80030c7cdb6238ede14bb5e6
Satpreet Makhija <sm.satpreet@gmail.com>
2021-12-05 15:32:51 +0530

    idk who changed the files

--- sender.py
+++ sender.py
@@ -6,7 +6,6 @@ import os
 import socket
 import time
 
-<<<<<<< HEAD
 # Creating a socket.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((socket.gethostname(), 22222))
@@ -24,20 +23,6 @@ file_size = os.path.getsize(file_name)
 client.send(file_name.encode())
 client.send(str(file_size).encode())
 
-=======
-
-PORT = 2223
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((socket.gethostname(), PORT))
-print("[INFO] Connected to server")
-file_name = "dataset.csv"
-file_size = os.path.getsize(file_name)
-
-print("[INFO] sending file")
-client.send(str(file_size).encode())
-
-
->>>>>>> a5eb0ee006fc01cb20802a3fe712408b920c3eb8
 # Opening file and sending data.
 with open(file_name, "rb") as file:
     c = 0
@@ -55,12 +40,6 @@ with open(file_name, "rb") as file:
     # Ending the time capture.
     end_time = time.time()
 
-<<<<<<< HEAD
 print("File Transfer Complete.Total time: ", end_time - start_time)
 # Cloasing the socket.
 sock.close()
-=======
-print("[INFO] File Transfer Complete.Total time: ", end_time - start_time)
-# Closing the socket.
-client.close()
->>>>>>> a5eb0ee006fc01cb20802a3fe712408b920c3eb8
